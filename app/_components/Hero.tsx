6b75d28d0c65f4c3e96b80cb4d08a7bbe88eac45
Ception <ception@live.ca>
2023-12-08 22:51:51 -0500

    Add imports and button component to Navbar Add Hero component to Home page Update title in Strapi index.html Update next.config.js for image domains

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
+      <div className="mb-8 flex flex-wrap justify-between md:mb-16">
+        <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+          <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
+            High quality Cannabis, at your fingertips!
+          </h1>
+          <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
+            We sell only the best quality cannabis products. We are the best in
+            the business
+          </p>
+          <button className="mt-4 bg-primary text-white py-2 px-4 rounded hover:bg-sky-600 transition">
+            SHOP OUR CATALOG
+          </button>
+        </div>
+        <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
+          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-xl bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+            <Image
+              src="https://images.unsplash.com/photo-1536795335207-28f63e2352f0?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Joint held up"
+              priority={true}
+              width={500}
+              height={500}
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div className="overflow-hidden rounded-xl bg-gray-100 shadow-lg">
+            <Image
+              src="https://images.unsplash.com/photo-1626106576760-d64336d3fa5b?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="weed nug"
+              priority={true}
+              width={500}
+              height={500}
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import { ShoppingBag } from "lucide-react";
 
 const links = [
   { name: "Home", href: "/" },
@@ -11,17 +14,18 @@ const links = [
 ];
 
 export default function Navbar() {
+  const [isHover, setHover] = useState(false);
   const pathName = usePathname();
   return (
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
-          <h1 className="text-4xl">
+          <h1 className="md:text-4xl text-2xl">
             ALOHA
             <span className="text-primary">KUSH</span>
           </h1>
         </Link>
-        <nav className="hidden gap-8 lg:flex 2xl:ml-16">
+        <nav className="hidden gap-6 lg:flex 2xl:ml-16">
           {links.map((link, index) => (
             <div key={index}>
               {pathName === link.href ? (
@@ -40,8 +44,16 @@ export default function Navbar() {
           ))}
         </nav>
 
-        <div className="flex divide-x border-r sm:border-l">
-            
+        <div className="flex divide-x">
+          <Button
+            variant={"link"}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            <ShoppingBag color={isHover ? "#0284c7" : "#fe8c00"} />
+            <span className="hidden text-xs text-gray-500 sm:block">Cart</span>
+          </Button>
         </div>
       </div>
     </header>
