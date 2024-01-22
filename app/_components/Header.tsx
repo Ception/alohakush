c4f392cf2264edff5465ab9d1ba500d28c9a2394
Ception <ception@live.ca>
2024-01-22 14:24:12 -0500

    Update Header styling and package dependencies

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 w-full z-50 bg-white border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
-          <h1 className="md:text-4xl text-2xl">
+          <h1 className="md:text-4xl text-2xl text-black">
             ALOHA
             <span className="text-primary">KUSH</span>
           </h1>
