f6265579fd8f9c641e10272d9ac21c47c40ba050
Ception <ception@live.ca>
2024-01-27 17:34:18 -0500

    Remove unused variables and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 import Hero from "./_components/Hero";
 
-export const isDevelopment = process.env.ENABLE_DEV === "true";
-export const API = isDevelopment
-  ? process.env.API_URL_DEV
-  : process.env.API_URL;
-
-export const AUTH_TOKEN = isDevelopment
-  ? process.env.API_TOKEN_DEV
-  : process.env.API_TOKEN;
-
-export const SITE_LINK = process.env.SITE_URL;
-
 export default function Home() {
   return (
     <>
