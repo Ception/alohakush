27f378453c76507f0ea9ac792bc4177e39107ced
Ception <ception@live.ca>
2024-01-27 22:26:06 -0500

    added no-cache

--- dist/server/next-font-manifest.js
+++ dist/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ception/Documents/repos.nosync/alohakush/app/layout\":[\"static/media/f884d4ea94220255-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/ception/Documents/repos.nosync/alohakush/app/layout":["static/media/f884d4ea94220255-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
